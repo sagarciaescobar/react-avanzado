4e79e0470c5c1c01ef6d07f3f39fd88e506b8916
sagarciaescobar <sa.garciaescobar@gmail.com>
2022-07-07 20:28:48 -0500

    add: service worker

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useParams } from 'react-router-dom'
 import { Layout } from '../../components/Layout'
-import { ListOfCategories } from '../../components/ListOfCategories'
+import { ListOfCategoriesComponent } from '../../components/ListOfCategories'
 import { ListOfPhotoCards } from '../../components/ListOfPhotoCards'
 
 export const Home = () => {
@@ -12,7 +12,7 @@ export const Home = () => {
       title='Tu app de fotos de mascotas' subtitle='Con petgram puedes encontrar fotos
       de tu animal favorito'
     >
-      <ListOfCategories />
+      <ListOfCategoriesComponent />
       <ListOfPhotoCards categoryId={id} />
     </Layout>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -15,6 +18,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - tu app fotos para mascotas',
+      shortname: 'Petgram',
+      description: 'Con petgram puedes encontrar fotos de animales domesticos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/png/icon.png'),
+          size: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: /https:\/\/(res.cloudinary.com|images.unsplash.com)/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: /https:\/\/petgram-server-sergio-opal.vercel.app\//,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
